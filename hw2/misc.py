3b9c183d0d8ebd695f06ea6d4df159e38462388b
Hexiang Hu <frank.hexiang@gmail.com>
2015-07-30 22:11:23 -0700

    Finish hw2/part0-XOR

--- hw2/misc.py
+++ hw2/misc.py
@@ -5,9 +5,9 @@
 from numpy import *
 
 def random_weight_matrix(m, n):
-    #### YOUR CODE HERE ####
+  #### YOUR CODE HERE ####
+  A0 = random.randn(m, n)
 
-
-    #### END YOUR CODE ####
-    assert(A0.shape == (m,n))
-    return A0
+  #### END YOUR CODE ####
+  assert(A0.shape == (m,n))
+  return A0
